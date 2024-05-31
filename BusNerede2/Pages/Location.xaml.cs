f5fc56da574a370de8a041bd16f2fd2ef4f3c704
M7mmedHomsi <m7mmedhomsi@gmail.com>
2024-05-31 21:44:25 +0300

    Children Page done

--- BusNerede2/Pages/Location.xaml.cs
+++ BusNerede2/Pages/Location.xaml.cs
@@ -1,9 +1,48 @@
-namespace BusNerede2;
+using Microsoft.Maui.Controls;
 
-public partial class Location : ContentPage
+namespace BusNerede2
 {
-	public Location()
-	{
-		InitializeComponent();
-	}
+    public partial class Location : ContentPage
+    {
+        public Location()
+        {
+            InitializeComponent();
+            ChildrenPicker.SelectedIndexChanged += OnPickerSelectedIndexChanged;
+        }
+
+        private void OnPickerSelectedIndexChanged(object sender, EventArgs e)
+        {
+            // Get the selected child
+            string selectedChild = ChildrenPicker.SelectedItem as string;
+
+        
+
+            // Update the UI or navigate based on the selection
+            // For example:
+             if (selectedChild == "Mohammed Homsi") {
+                childImage.Source = "childee.png";
+                bus.Source = "arrived.png";
+                note.Text = "Arrived Safely";
+                note.TextColor = Color.FromRgb(0, 255, 0);
+                dep.Text = "15:00";
+                arr.Text = "17:00";
+            }
+             else if (selectedChild == "Yasmeen Homsi") {
+                childImage.Source = "childee2.png";
+                bus.Source = "otw.png";
+                note.Text = "On the Way!Stuck in Traffic";
+                note.TextColor = Color.FromRgb(255, 165, 0);
+                dep.Text = "15:00";
+                arr.Text = "-";
+            }
+             else if (selectedChild == "Majed Homsi") {
+                childImage.Source = "childee3.png";
+                bus.Source = "nobus.png";
+                note.Text = "Did not Attend";
+                note.TextColor = Color.FromRgb(255, 0, 0);
+                dep.Text = "-";
+                arr.Text = "-";
+            }
+        }
+    }
 }
